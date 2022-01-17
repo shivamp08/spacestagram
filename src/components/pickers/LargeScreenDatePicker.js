c937caf1f81a2355200c27c24e59e9eaa30c6f9b
Shivam Patel <shivam08@my.yorku.ca>
2022-01-17 01:23:52 -0500

    add timeout to date picker

--- src/components/pickers/LargeScreenDatePicker.js
+++ src/components/pickers/LargeScreenDatePicker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 import TextField from "@mui/material/TextField";
 
@@ -11,13 +11,18 @@ const LargeScreenDatePicker = ({
   minDate,
   handleDateChange,
 }) => {
-  const onDateChange = (val) => {
-    console.log(val);
-    if (val) {
-      let date = new Date(getDateAsISOString(val) + "T10:20:30Z");
-      handleDateChange(date);
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    if (date) {
+      const delay = setTimeout(() => {
+        handleDateChange(new Date(getDateAsISOString(date) + "T10:20:30Z"));
+      }, 800);
+
+      return () => clearTimeout(delay);
     }
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date]);
 
   return (
     <DesktopDatePicker
@@ -25,7 +30,7 @@ const LargeScreenDatePicker = ({
       inputFormat="yyyy-MM-dd"
       mask="____-__-__"
       value={value}
-      onChange={onDateChange}
+      onChange={(val) => setDate(val)}
       maxDate={maxDate}
       minDate={minDate}
       renderInput={(params) => <TextField {...params} />}
--- src/components/pickers/MobileScreenDatePicker.js
+++ src/components/pickers/MobileScreenDatePicker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import MobileDatePicker from "@mui/lab/MobileDatePicker";
 import TextField from "@mui/material/TextField";
 
@@ -11,12 +11,14 @@ const LargeScreenDatePicker = ({
   minDate,
   handleDateChange,
 }) => {
-  const onDateChange = (val) => {
-    if (val) {
-      let date = new Date(getDateAsISOString(val) + "T10:20:30Z");
-      handleDateChange(date);
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    if (date) {
+      handleDateChange(new Date(getDateAsISOString(date) + "T10:20:30Z"));
     }
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date]);
 
   return (
     <MobileDatePicker
@@ -24,7 +26,8 @@ const LargeScreenDatePicker = ({
       inputFormat="yyyy-MM-dd"
       mask="____-__-__"
       value={value}
-      onChange={onDateChange}
+      onAccept={(val) => setDate(val)}
+      onChange={() => setDate(null)}
       maxDate={maxDate}
       minDate={minDate}
       renderInput={(params) => (
